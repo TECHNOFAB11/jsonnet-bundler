43de744f094f87de8209102f5bb9d4df4c03bc4b
Technofab <admin@technofab.de>
2023-05-23 18:30:41 +0200

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A jsonnet package manager";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs, systems }:
+  let
+    version = "0.5.1";
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    nixpkgsFor = forEachSystem (system: import nixpkgs { inherit system; });
+  in {
+    # Provide some binary packages for selected system types.
+    packages = forEachSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        jb = pkgs.buildGoModule {
+          pname = "jsonnet-bundler";
+          inherit version;
+
+          src = ./.;
+          CGO_ENABLED = 0;
+
+          ldflags = [ "-s" "-w" "-X main.Version=${version}" ];
+
+          vendorSha256 = "sha256-5a+8emtKjOlSr4V9P2YW5u0FVy9to5rIVfF9i90BCe4=";
+
+          doCheck = false;
+        };
+      }
+    );
+
+    defaultPackage = forEachSystem (system: self.packages.${system}.jb);
+  };
+}
